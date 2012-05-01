745e01eff4ca143d58712f915dd5694e9dd4dca8
Daniel Davey <whoisdanieldavey@gmail.com>
2012-05-01 15:37:27 -0700

    added root redirect to /new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Appdroneapp::Application.routes.draw do
-  root to: 'app_template#about'
+  #root to: 'app_template#about'
+  root to: redirect('/new')
   get :new, to: 'ember#new'
   get :build, to: 'app_template#build'
   get '/:uuid(.:format)', to: 'app_template#show', as: :app_template
