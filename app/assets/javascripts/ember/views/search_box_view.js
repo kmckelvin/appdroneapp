e1dee40fc41666f673ed3a95c00c9be93d08474e
Daniel Davey <daniel@platform45.com>
2012-05-04 11:50:41 +0200

    Added ellipsis to search box.

--- app/assets/javascripts/ember/views/search_box_view.js
+++ app/assets/javascripts/ember/views/search_box_view.js
@@ -1,5 +1,5 @@
 App.SearchBoxView = Ember.TextField.extend({
-  placeholder: 'Search',
+  placeholder: 'Search&hellip;',
   valueBinding: 'App.dronesController.searchQuery',
   classNames: 'input-small search-query'
 });
